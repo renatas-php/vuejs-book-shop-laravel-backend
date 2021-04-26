938a700ea39210f6ed84c870e2433ca34bc6dd37
renatas-php <rene.programuotojas@gmail.com>
2021-04-26 14:15:51 +0300

    s

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,36 @@
     </head>
     <body>
 
+        <div class="top-nav">
+            <ul>
+                <li><a href="">Pradinis</a></li>
+                <li><a href="" id="categories">Kategorijos <i class="fas fa-chevron-down"></i></a>
+                    <ul class="dropdown">
+                        <li><a href="">Nekazas</a></li>
+                        <li><a href="">Kzazas</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Populiariausios</a></li>
+                <li><a href="" class="login-reg">Prisijungti</a></li>
+            </ul>
+        </div>
+
         <div id="app">
             <books-index></books-index>
         </div>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" crossorigin="anonymous" />
         <script src="{{ asset('js/app.js') }}"></script>
+        <script>
+            let cats = document.getElementById('categories');
+            let dropdown = document.querySelector('.dropdown');
+            cats.addEventListener('mouseover', function() {
+                dropdown.classList.add('active-dropdown');
+                document.querySelector('.fas').style.transform = 'rotateZ(180deg)';
+            });
+            dropdown.addEventListener('mouseleave', function() {
+                dropdown.classList.remove('active-dropdown');
+                document.querySelector('.fas').style.transform = 'rotateZ(0deg)';
+            });            
+        </script>
     </body>
 </html>
